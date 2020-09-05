961df64d8c3d29619937402df14b49c9ed8cbd5a
Kishore kunal <33398097+hesoyamm@users.noreply.github.com>
2020-09-05 18:57:18 +0530

    Update Different_Names.cpp
    This code is much more faster. BTW:Your repo is very helpful. thanks.

--- Language Tools/Different_Names.cpp
+++ Language Tools/Different_Names.cpp
@@ -19,41 +19,40 @@ Sample Output:
 -1 */
 #include<bits/stdc++.h>
 using namespace std;
-vector<string> splitter(string s)
-{
-	vector<string>v;
-	int start=0;
-	int end=0;
-	while(end<s.length())
-	{
-		while(s[end]!=' ')
-		{
-			end++;
-		}
-		v.push_back(s.substr(start, end-start));
-		end++;
-		start=end;
-	}
-	return v;
-}
-int main()
-{
-	string s;
-	getline(cin, s);
-	map<string, int>m;
-	vector<string>v=splitter(s);
-	for(int i=0; i<v.size(); i++)
+#include<string>
+int alag(string s,map<string,int> m ){
+
+	std::istringstream iss(s);
+	std::vector<std::string> v((std::istream_iterator<std::string>(iss)),
+                                 std::istream_iterator<std::string>());
+                                    
+                                    
+    for(int i=0; i<v.size(); i++)
 	{
 		m[v[i]]+=1;
 	}
-	map<string, int>::iterator it;
-	for(it=m.begin(); it!=m.end(); it++)
-	{
-		string name=it->first;
-		int freq=it->second;
-		if(freq>1)
-		{
-			cout<<name<<" "<<freq<<endl;
-		}
-	}
+	map<string,int>::iterator it;
+	for(it =m.begin();it!=m.end();it++){
+	    
+	    string name = it->first;
+	    int freq = it->second;
+	    if(freq>1){
+	        cout<<name<<" "<<freq<<endl;
+	    }
+	}}
+int main ()
+{	
+	string s ; 
+	//getline 
+	getline(cin,s);
+	map<string,int> m;
+	alag(s,m);
+
+
+
+
+
+	return 0;
+
+
 }
